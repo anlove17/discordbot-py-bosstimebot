c3b4905fd210e73b5a9f023eb4a837b826406492
jihun9393 <69469014+jihun9393@users.noreply.github.com>
2023-04-04 02:08:34 +0900

    디스코드 보스타임 봇 컷 기능 추가

--- discordbot.py
+++ discordbot.py
@@ -12,18 +12,27 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}.')
+    print('봇이 온라인으로 전환되었습니다.')
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("보스 타임 체크"))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    if message.content.startswith('!컷'):
+        target = message.content.split()[1] #입력된 메시지에서 2번째 단어 추출
+        now = datetime.utcnow() + timedelta(hours=12) #KST (UTC+9)
 
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
-
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+        reply = []
+        for name, info in BOSS_INFO.items():
+            if name == target:
+                next_spawn = now + timedelta(minutes=info["젠주기"])
+                BOSS_INFO[name]["다음 젠 시간"] = next_spawn.strftime('%H:%M')
+                reply.append(f"{next_spawn.hour}:{next_spawn.minute}, {name}, , {info['젠위치']}, {info['레벨']}")
+            else:
+                reply.append(f"{info['다음 젠 시간']} , {name},  {info['젠위치']}, {info['레벨']}")
+        await message.reply('\n'.join(reply))
 
 
 try:
